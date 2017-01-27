6978d5d9d51b430d8c9715049369dadd082e8916
Dennis Eusebio <dennis@thoughtandtheory.com>
2017-01-26 21:25:49 -0500

    update assignment_26

--- scripts/album.js
+++ scripts/album.js
@@ -45,7 +45,7 @@
   var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -76,9 +76,25 @@
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
+
+  var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+  var songRows = document.getElementsByClassName('album-view-song-item');
+  var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
  
  window.onload = function() {
     setCurrentAlbum(albumPicasso);
+
+    songListContainer.addEventListener('mouseover', function(event) {
+        if (event.target.parentElement.className === 'album-view-song-item') {
+            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+        }
+     });
+
+    //for (var i = 0; i < songRows.length; i++) {
+    //    songRows[i].addEventListener('mouseleave', function(event) {
+    //         this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+    //    });
+    //}
 
     var albumArray = [albumPicasso,albumMarconi,albumAlGreen];
     var index = 1;
